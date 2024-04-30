9e490858bb7c833292b7df0e86b9bb58f9288b9e
debasihs <dev.dey161@gmail.com>
2024-04-30 11:40:47 +0530

    application level singleton

--- app/src/main/java/com/example/daggerapplication/UserRegistrationComponent.kt
+++ app/src/main/java/com/example/daggerapplication/UserRegistrationComponent.kt
@@ -2,7 +2,9 @@ package com.example.daggerapplication
 
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [UserRepositoryModule::class,NotificationServiceModule::class])
 interface UserRegistrationComponent {
 
